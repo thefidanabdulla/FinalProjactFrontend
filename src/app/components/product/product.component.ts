722f4df92dfed345410f943149445ca60e54afce
thefidanabdulla <thefidanabdulla@gmail.com>
2022-07-01 23:31:04 +0400

    all problems solved finally

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,34 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
 import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-
 export class ProductComponent implements OnInit {
 
-  products: Product[] = [];
-  // productResponseModel:ProductResponseModel = {};
-  apiUrl = "https://localhost:44386/api/products/getall";
-
-  constructor(private httpClient: HttpClient) {
-
-  }
+  products:Product[] = [];
+  apiUrl = 'https://localhost:44386/api/products/getall';
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
-    console.log("Init calisti")
+    this.getProducts();
+    console.log(this.products)
   }
-  
+
   getProducts() {
     this.httpClient
-    .get<ProductResponseModel>(this.apiUrl)
-    .subscribe(response => {
-      this.products = response.data
-    });
-    console.log("dasdas")
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        console.log(response)
+        this.products = response.data
+      });
   }
 }
