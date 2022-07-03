da45edcadbf1a98765596fb4a51942dffc41c842
thefidanabdulla <thefidanabdulla@gmail.com>
2022-07-03 21:23:53 +0400

    .

--- src/app/services/category/category.service.ts
+++ src/app/services/category/category.service.ts
@@ -1,11 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/models/category';
+import { ListResponseModel } from 'src/app/models/listResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
-  apiUrl=''
+
+  apiUrl='https://localhost:44386/api/categories/getall'
+
   constructor(private httpClient:HttpClient) { }
 
+  getCategories():Observable<ListResponseModel<Category>>{
+    return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
+  }
 }
