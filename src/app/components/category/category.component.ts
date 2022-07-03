5c75bccb50fb09100b617e7444504c2b3b5f96ff
thefidanabdulla <thefidanabdulla@gmail.com>
2022-07-03 21:43:29 +0400

    category is done

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category/category.service';
 
 @Component({
   selector: 'app-category',
@@ -8,9 +9,16 @@ import { Category } from 'src/app/models/category';
 })
 export class CategoryComponent implements OnInit {
   categories:Category[] = []
-  constructor() { }
+  dataLoaded = false;
+  constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
   }
-
+  getCategories(){
+    this.categoryService.getCategories().subscribe(response => {
+      this.categories = response.data;
+      this.dataLoaded = true;
+    })
+  }
 }
