e6c3d9cbd6156d453e80ad70173d3089ed8c00a7
thefidanabdulla <thefidanabdulla@gmail.com>
2022-07-04 21:53:11 +0400

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ProductComponent } from './components/product/product.component';
 const routes: Routes = [
   {path:'', pathMatch:"full", component:ProductComponent},
   {path:'products', component:ProductComponent},
+  {path:'products/category/:categoryId', component:ProductComponent},
   
 ];
 
